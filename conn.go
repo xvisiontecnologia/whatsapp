b7fcf7ba15a269f2af424cbfaa9ff1e0199f133b
Eulelis <eulelis@xvisiontecnologia.com.br>
2020-06-23 19:47:30 -0300

    Atualização

--- conn.go
+++ conn.go
@@ -135,8 +135,8 @@ func NewConnWithProxy(timeout time.Duration, proxy func(*http.Request) (*url.URL
 		msgTimeout: timeout,
 		Store:      newStore(),
 
-		longClientName:  "github.com/xvisiontecnologia/whatsapp",
-		shortClientName: "go-whatsapp",
+		longClientName:  "Xpro",
+		shortClientName: "Xpro",
 		clientVersion:   "0.1.0",
 		Proxy:           proxy,
 	}
@@ -156,8 +156,8 @@ func (wac *Conn) connect() (err error) {
 	}()
 
 	dialer := &websocket.Dialer{
-		ReadBufferSize:   25 * 1024 * 1024,
-		WriteBufferSize:  10 * 1024 * 1024,
+		ReadBufferSize:   0,
+		WriteBufferSize:  0,
 		HandshakeTimeout: wac.msgTimeout,
 		Proxy:            wac.Proxy,
 	}
